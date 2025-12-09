eef66115bb22809580def168e696b9a0fd7158db
Ahmed-469 <ahmed3almanoo@gmail.com>
2025-12-09 20:46:18 +0300

    Add match checking for card flips

--- app.js
+++ app.js
@@ -6,10 +6,46 @@ const startButtonElement = document.querySelector('#StartButton');
 
 let lockCards = true;
 let randomCards;
+let firstCard = null
+let secondCard = null
 
-function flipCards(card){ 
+function flipCard(card){ 
     if(lockCards) return;
     card.classList.add('flip');
+    if(firstCard && secondCard) return;
+    else if(!firstCard){
+        firstCard = card; 
+        return;
+    }
+    secondCard = card;
+    lockCards = true;
+    checkCards()
+}
+
+function checkCards() {
+    let firstEmoji = firstCard.querySelector('.back').textContent;
+    let secondEmoji = secondCard.querySelector('.back').textContent;
+    if (firstEmoji === secondEmoji) {
+        firstCard = null;
+        secondCard = null;
+        lockCards = false;
+    } 
+    else {
+        setTimeout(function(){
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        firstCard = null;
+        secondCard = null;
+        lockCards = false;
+        } , 500);
+    }
+}
+
+function previewCards(){cardElements.forEach(function(card){
+    card.classList.add('flip');
+    });
+    setTimeout(resetCards, 2000);
+    randomizeCards();
 }
 
 function resetCards(){ cardElements.forEach(function(card) {
@@ -18,21 +54,14 @@ function resetCards(){ cardElements.forEach(function(card) {
     lockCards = false;
 }
 
-function previewCards(){cardElements.forEach(function(card){
-    card.classList.add('flip');
-    });
-    setTimeout(resetCards, 2000);
-    shuffleCards()
-}
-
-function shuffleCards(){ cardElements.forEach(function(card){
+function randomizeCards(){ cardElements.forEach(function(card){
     randomCards = Math.floor(Math.random() * 12);
     card.style.order = randomCards;
 })}
 
 cardElements.forEach(function(card) { 
     card.addEventListener('click', function() {
-        flipCards(card);
+        flipCard(card);
     });
 });
 
